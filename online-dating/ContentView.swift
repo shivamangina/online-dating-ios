70450fa77503ffa7aecaea52396f7fb7ddc05b58
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2025-01-25 15:36:46 +0530

    Initial Commit

---
+++ online-dating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  online-dating
+//
+//  Created by Shiva Kumar Mangina on 25/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ online-dating/online_datingApp.swift
@@ -0,0 +1,17 @@
+//
+//  online_datingApp.swift
+//  online-dating
+//
+//  Created by Shiva Kumar Mangina on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct online_datingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
